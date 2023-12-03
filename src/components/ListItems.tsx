71f6321035493e9f83e6afc81f188aa815f868a0
Jacek Liman <limaniec@gmail.com>
2023-12-03 09:37:55 +0100

    formatting useContext hoook

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -4,31 +4,28 @@ import ListItem from "./ListItem";
 import { CgSpinner } from "react-icons/cg";
 
 const ListItems = () => {
-  const contextValues = useContext(HomeContext);
+  const { houses,loading } = useContext(HomeContext);
 
-  if (contextValues) {
-    if (contextValues.loading) {
-      return (
-        <section className="flex items-center justify-center h-[600px] mt-5">
-          <CgSpinner className="animate-spin text-violet-700" size={40} />
-        </section>
-      );
-    }
+  if (loading) {
+    return (
+      <section className="flex items-center justify-center h-[600px] mt-5">
+        <CgSpinner className = "animate-spin text-violet-700" size={40}/>
+      </section>
+    );
+  }
 
-     if (contextValues.houses.length < 1) {
-       return (
-         <section className="flex items-center justify-center h-[600px] mt-5">
-           NO DATA
-         </section>
-       );
-     }
-
+  if (houses.length < 1) {
+    return (
+      <section className="flex items-center justify-center h-[600px] mt-5">
+        NO DATA
+      </section>
+    );
   }
 
   return (
     <section className="pt-80 lg:pt-20 ">
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:gap-x-10 gap-x-8 gap-y-10 lg:grid-cols-3">
-        {contextValues?.houses.map((house) => {
+        {houses.map((house) => {
           return <ListItem key={house.id} house={house} />;
         })}
       </ul>
--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -4,9 +4,9 @@ import { HomeContext } from "./context/HomeContext";
 import { HiMagnifyingGlass } from "react-icons/hi2";
 const SearchButton = () => {
 
-  const contextValues = useContext(HomeContext)
+  const {handleClickSearch} = useContext(HomeContext)
   return (
-    <button onClick={contextValues?.handleClickSearch}  className="text-white px-10 py-3  rounded-xl bg-violet-700 font-semibold">
+    <button onClick={handleClickSearch}  className="text-white px-10 py-3  rounded-xl bg-violet-700 font-semibold">
       <HiMagnifyingGlass />
     </button>
   );
